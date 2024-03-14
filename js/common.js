103c596f71761c6e8e2c9af6cd9476864120cb49
s2yeji <s2seolyeji@gmail.com>
2024-03-14 18:07:12 +0900

    re

---
+++ js/common.js
@@ -0,0 +1,23 @@
+let mainSlide = new Swiper(".mainSlide", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".next1",
+    prevEl: ".prev1",
+  },
+  pagination: {
+    el: ".pg1",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
+
+let eduSlide = new Swiper(".eduSlide", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  speed: 1000,
+});
